64461977a5e260c16abe68c8830dce1c1b79271f
rwarner <rwarner@mymail.mines.edu>
2019-09-05 11:53:41 -0600

    Setting enum

---
+++ src/Setting.java
@@ -0,0 +1,16 @@
+
+public enum Setting{
+	OFF("---"),LOW("--+"),MEDIUM("-++"),HIGH("+++");
+	
+	private String value;
+	
+	@Override
+	public String toString() {
+		return this.value;
+	}
+	
+	private Setting(String value) {
+		this.value = value;
+	}
+	
+}
